04e22f15bf04c3ecb8001a92cf6310aa96ce851a
itsrakshya09 <rakshya.242028@ncit.edu.np>
2026-06-17 19:52:21 +0545

    made card .jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
-// Day 5: conditional Rendering
-import { useState } from 'react'
+// Day 6 : using list and .map()
+import Card from "./Card.jsx"
 function App(){
-const[isloggedIn, setloggedIn]=useState(false) //the start valiue is false
+    const fruit = ['Apple','Mango','orange']  //we made an array
+    return (
+        <div>
+            <Card/>
+            <h1>Fruits List</h1>
+            <ul>
+                {fruit.map((fruit,index) => (   /**map() is a js function that loops thru every item and fruit is current item value*/
+                    <li key ={index}> {fruit} </li>
+                ) )}
+            </ul>
 
-return(
-    <div>
-    {isloggedIn? (<h1>Welcome</h1> ) : ( <h1>Please login</h1>)}
-    <button onClick={() => setloggedIn(!isloggedIn)}>
-        {isloggedIn ? 'logout':'Login'}
-        </button>
-    </div>
+        </div>
 
     )
 }
+
 export default App
 
 
@@ -29,18 +33,48 @@ export default App
 
 
 
+// Day 5 home work
+// import { useState } from 'react'
+// function Card(props){
+//     return(
+//         <div>
+//             <h1>{props.Sport}</h1>
+//         </div>
+//     )
+// }
+// function App(){
+// const[count,setCount]=useState(0)
+
+// return(
+//     <div>
+//         <Card Sport= 'Football' />
+//         <h2>Like:{count}</h2>
+//         <button onClick={() => setCount(count+1)}>Like</button>
+//         <button onClick={() => setCount(count-1)}>Dislike</button>
+//         { count>20 ? (<h3>Popular</h3>) : (<h3>Not popular</h3>)}
+//     </div>
+// )
+// }
+// export default App
 
 
 
+// // Day 5: conditional Rendering
+// import { useState } from 'react'
+// function App(){
+// const[isloggedIn, setloggedIn]=useState(false) //the start valiue is false
 
+// return(
+//     <div>
+//     {isloggedIn? (<h1>Welcome</h1> ) : ( <h1>Please login</h1>)}
+//     <button onClick={() => setloggedIn(!isloggedIn)}>
+//         {isloggedIn ? 'logout':'Login'}
+//         </button>
+//     </div>
 
-
-
-
-
-
-
-
+//     )
+// }
+// export default App
 
 
 
---
+++ src/Card.jsx
@@ -0,0 +1,6 @@
+function Card(){
+    return(
+        <h1>Hello card</h1>
+    )
+}
+export default Card
